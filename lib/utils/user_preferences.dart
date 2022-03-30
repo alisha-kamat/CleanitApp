ce43e7b1f0acdd5f1de9d4a4a812d563a0828660
Alisha Kamat <84401192+alisha-kamat@users.noreply.github.com>
2022-03-30 14:38:10 +0530

    Added files

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import '../model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+    'https://media.istockphoto.com/photos/headshot-portrait-of-smiling-female-employee-posing-in-office-picture-id1318858332?k=20&m=1318858332&s=612x612&w=0&h=MQk8yiNlqpigOKaC6Uam8bhAir5HKpHzQLYRwCnUCLM=',
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com',
+    about:
+    'Certified Nutritionist with years of experience in creating effective diets and training plans focused on achieving individual customers goals.',
+    isDarkMode: false,
+  );
+}
